1147671c762ab1631102518e3774195a8668a040
wzj <wzj@3ac5beaa-6aae-4643-96fb-d167a1637ba8>
2022-05-27 09:19:41 +0000

    git-svn-id: https://39.108.144.9:8443/svn/fdx_crm@3 3ac5beaa-6aae-4643-96fb-d167a1637ba8

--- 1.0.0/web/Application/Common/Conf/config.php
+++ 1.0.0/web/Application/Common/Conf/config.php
@@ -67,7 +67,7 @@ return [
     'SHOW_ERROR_MSG' => true,
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => '39.108.144.9', // 服务器地址
-    'DB_NAME' => 'crm8000', // 数据库名
+    'DB_NAME' => 'fdx_crm', // 数据库名
     'DB_USER' => 'feibukeji', // 用户名
     'DB_PWD' => 'ladfjeuwf71741s', // 密码
     'DB_PORT' => '3306' // 端口
