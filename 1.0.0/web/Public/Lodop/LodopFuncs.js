fd0d28b3e4b8a912e065daabb29bb2f15a7df754
wzj <wzj@3ac5beaa-6aae-4643-96fb-d167a1637ba8>
2022-08-01 09:57:55 +0000

    git-svn-id: https://39.108.144.9:8443/svn/fdx_crm@82 3ac5beaa-6aae-4643-96fb-d167a1637ba8

--- 1.0.0/web/Public/Lodop/LodopFuncs.js
+++ 1.0.0/web/Public/Lodop/LodopFuncs.js
@@ -47,7 +47,7 @@ if (needCLodop()) {
 	var head = document.head || document.getElementsByTagName("head")[0]
 			|| document.documentElement;
 	var oscript = document.createElement("script");
-	oscript.src = "http://localhost:8443/CLodopfuncs.js?priority=1";
+	oscript.src = "http://localhost:8000/CLodopfuncs.js?priority=1";
 	head.insertBefore(oscript, head.firstChild);
 /*
 	// 引用双端口(8000和18000）避免其中某个被占用：
