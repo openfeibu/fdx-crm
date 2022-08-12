46449d8829225c71556c852081f9498d943aa34e
wzj <wzj@3ac5beaa-6aae-4643-96fb-d167a1637ba8>
2022-08-12 07:40:30 +0000

    git-svn-id: https://39.108.144.9:8443/svn/fdx_crm@115 3ac5beaa-6aae-4643-96fb-d167a1637ba8

--- 1.0.0/web/Public/Scripts/PSI/Customer/CustomerEditForm.js
+++ 1.0.0/web/Public/Scripts/PSI/Customer/CustomerEditForm.js
@@ -181,8 +181,9 @@ PCL.define("PSI.Customer.CustomerEditForm", {
         }, {
           id: "PSI_Customer_CustomerEditForm_editMobile01",
           fieldLabel: "手机",
-          allowBlank: true,
+          allowBlank: false,
           blankText: "没有输入客户名称",
+          beforeLabelTextTpl: PSI.Const.REQUIRED,
           name: "mobile01",
           value: entity == null ? null : entity
             .get("mobile01"),
